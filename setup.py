e1d6597b6d7082725148b7159b6814a9c374e0a4
Dhingra93 <rohitdhingra93@yahoo.com>
2024-11-14 18:38:48 +0530

    First setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HyponDoTE="-e ."
+
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements. 
@@ -9,6 +11,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()  ## reading file
         requirements=[req.replace('\n','') for req in requirements]     
+
+        if HyponDoTE in requirements:
+            requirements.remove() 
+
 
     return requirements
 
