286d46dfe463a183945cc752ba307044d3f63308
Dhingra93 <rohitdhingra93@yahoo.com>
2024-11-15 00:21:46 +0530

    Exception Handling Completed

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from src.ML_PROJECT.logger import logging
-from src.ML_PROJECT.exception import test
+from src.ML_PROJECT.exception import CustomException
+
+import sys
 
 if __name__=="__main__":
     logging.info("The execution has started")
-    print(test)
+    
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("CustomException")
+        raise CustomException(e,sys)
